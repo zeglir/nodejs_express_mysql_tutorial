8967ed69dfef7ac0dc5b22fab0a19548c22c1692
zeglir <zeglir@jcom.zaq.ne.jp>
2021-12-07 21:33:59 +0900

    2021/12/7 学習分

--- tastylog/app.js
+++ tastylog/app.js
@@ -1,2 +1,30 @@
 const port = process.env.PORT;
-console.log(`port number is ${port}`);
+const path = require("path");
+const favicon = require("serve-favicon");
+const express = require("express");
+const app = express();
+
+// Hello World 動作確認用
+// app.get("/", (req, res) => {
+//   res.end("Hello World!");
+// });
+
+// Expressで ejsを使う設定（おまじない）
+app.set("view engine", "ejs");
+
+// Expressで静的コンテンツ配信の設定
+// faviconを serve-faviconで配信
+app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+// publicフォルダ配下を /public として公開する
+// 先頭のパス指定は省略可能で、その場合は "/" を指定した場合と同じになる
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+// Expressで動的コンテンツ配信の設定
+// モジュール形式の route handler を使用
+app.use("/", require("./routes/index"));
+
+// アプリケーションを指定ポートで起動
+app.listen(port, () => {
+  console.log(`Application listening on port:${port}`);
+});
+
